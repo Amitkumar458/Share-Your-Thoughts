a930297d7406219ce87e90dc24902df6b7baa00c
Amit kumar <amitbhrampuriya@gmail.com>
2023-10-07 15:00:50 +0530

    backend api created

--- src/feature/posts/postSlice.jsx
+++ src/feature/posts/postSlice.jsx
@@ -4,7 +4,7 @@ const initialState = [
     {
         name: "Amit kumar",
         title: "We Must Love Once In Our Life",
-        description: "Love is a one of the best felling of our life. the feeling of that first love/crush...when the hormones and feelings used to touch the peak and there's comes a feeling which can never be expressed in words. Love is a quality that defines human beings. Love is a quality of the human heart. It is a feeling that the human species can express. It is love that brings and keeps people together in harmony and happiness.",
+        description: "Love is a one of the best feeling of our life. the feeling of that first love/crush...when the hormones and feelings used to touch the peak and there's comes a feeling which can never be expressed in words. Love is a quality that defines human beings. Love is a quality of the human heart. It is a feeling that the human species can express. It is love that brings and keeps people together in harmony and happiness.",
         date: "23-june-2023"
     }
 ]
