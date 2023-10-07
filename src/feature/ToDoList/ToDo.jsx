c6aea3d764b6345fb55fac1fe8a6638eaad02f4e
Amit kumar <amitbhrampuriya@gmail.com>
2023-10-07 18:40:51 +0530

    fixing error

--- src/feature/ToDoList/ToDo.jsx
+++ src/feature/ToDoList/ToDo.jsx
@@ -16,7 +16,7 @@ const ToDo = () => {
         setval(e.target.value);
     }
     return (
-        <div className='main'>
+        <div className='main App'>
                 <h1 style={{ color: 'red'}}>Welcome to To-Do List</h1>
             <div className='box2'>
                 <div style={{textAlign:'center'}}>
@@ -24,7 +24,7 @@ const ToDo = () => {
                 </div>
                 <form className="group" onSubmit={runthis}>
                     <input required="" type="text" onChange={changefun} placeholder='Enter Item' value={val} className="input1"/>
-                    <button type='submit' className='btn1'>Add</button>
+                    <button type='submit' className='btntodo'>Add</button>
                 </form>
                 <Showlist/>
             </div>
